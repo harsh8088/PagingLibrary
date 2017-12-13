9943d14119870aa6a074b76864a9393ff612ed5f
hrawat_xavient <harshrock2010@gmail.com>
2017-12-13 19:00:07 +0530

    code updated

--- app/src/main/java/com/hrawat/paginglibrary/User.java
+++ app/src/main/java/com/hrawat/paginglibrary/User.java
@@ -3,19 +3,23 @@ package com.hrawat.paginglibrary;
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.recyclerview.extensions.DiffCallback;
 
 import java.util.Objects;
 
 @Entity
 public class User {
+
     static DiffCallback<User> DIFF_CALLBACK = new DiffCallback<User>() {
         @Override
         public boolean areItemsTheSame(@NonNull User oldItem, @NonNull User newItem) {
             return oldItem.userId == newItem.userId;
         }
 
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
         @Override
         public boolean areContentsTheSame(@NonNull User oldItem, @NonNull User newItem) {
             return oldItem.equals(newItem);
@@ -28,13 +32,12 @@ public class User {
     public String firstName;
     public String address;
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public boolean equals(Object obj) {
         if (obj == this)
             return true;
-
         User user = (User) obj;
-
         return user.userId == this.userId && Objects.equals(user.firstName, this.firstName);
     }
 }
--- app/src/main/java/com/hrawat/paginglibrary/UserAdapter.java
+++ app/src/main/java/com/hrawat/paginglibrary/UserAdapter.java
@@ -9,8 +9,7 @@ import android.widget.TextView;
 
 public class UserAdapter extends PagedListAdapter<User, UserAdapter.UserItemViewHolder> {
 
-
-     public UserAdapter() {
+    UserAdapter() {
         super(User.DIFF_CALLBACK);
     }
 
@@ -23,22 +22,23 @@ public class UserAdapter extends PagedListAdapter<User, UserAdapter.UserItemView
 
     @Override
     public void onBindViewHolder(UserItemViewHolder holder, int position) {
-        User user= getItem(position);
-        if(user!=null) {
+        User user = getItem(position);
+        if (user != null) {
             holder.bindTo(user);
         }
     }
 
     static class UserItemViewHolder extends RecyclerView.ViewHolder {
+
         TextView userName, userId;
 
-        public UserItemViewHolder(View itemView) {
+         UserItemViewHolder(View itemView) {
             super(itemView);
             userId = itemView.findViewById(R.id.userId);
             userName = itemView.findViewById(R.id.userName);
         }
 
-        public void bindTo(User user) {
+         void bindTo(User user) {
             userName.setText(user.firstName);
             userId.setText(String.valueOf(user.userId));
         }
